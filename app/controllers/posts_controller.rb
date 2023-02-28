3cf66a27917d7db2f8310839e5b9489004d75043
Linus Shaddai <54178270+Linushaddai99@users.noreply.github.com>
2023-02-28 21:35:42 +0100

    Fix linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,5 @@
-class PostsController < ApplicationController 
-    def index
-      
-    end
+class PostsController < ApplicationController
+  def index; end
 
-    def show
-
-    end
+  def show; end
 end
