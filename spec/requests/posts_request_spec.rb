4749ea9675355d24c5294afad784e38a20b66d2c
Linus Shaddai <54178270+Linushaddai99@users.noreply.github.com>
2023-03-01 17:14:21 +0100

    Write tests for controller actions

---
+++ spec/requests/posts_request_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'PostsController', type: :request do
+    context 'get index for posts' do
+        before(:example) { get('/users/1/posts') }
+        it 'should display the correct placeholder' do 
+            expect(response.body).to include 'Here is a list of posts for a given user'
+        end
+    
+        it 'should have a status response of 200' do 
+            expect(response).to have_http_status(200)
+        end
+    
+        it "renders 'index' template" do
+            expect(response).to render_template('index')
+        end
+    end
+
+    context 'Get #show for posts' do
+        before(:example) { get('/users/1/posts/1') }
+        it 'should have a status response of 200' do
+          expect(response).to have_http_status(200)
+        end
+
+        it "renders 'show' template" do
+          expect(response).to render_template('show')
+        end
+
+        it 'should display the correct placeholder' do
+          expect(response.body).to include 'Here is a page for the details about a particuler post'
+        end
+    end
+end
