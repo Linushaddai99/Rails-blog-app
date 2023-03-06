95d5dc7a4c161f35b0bb1841195d2dc9341ffbe7
Linus Shaddai <54178270+Linushaddai99@users.noreply.github.com>
2023-03-06 13:52:45 +0100

    Refactor rcode

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,31 @@
-class CommentsController < ApplicationController
-  def new
-    @comment = Comment.new
-  end
+# class CommentsController < ApplicationController
+#   def create
+#     @comment = Comment.new
+#     @comment = Comment.new(text: params[:comment][:text], author_id: current_user.id, post_id: params[:post_id])
 
+#     if @comment.save
+#       redirect_to "/users/#{current_user.id}/posts/#{params[:post_id]}"
+#     else
+#       render 'posts/show'
+#     end
+#   end
+
+#   private
+
+#   def comment_params
+#     params.require(:comment).permit(:text)
+#   end
+# end
+
+class CommentsController < ApplicationController
   def create
-    @comment = Comment.new(text: params[:comment][:text], author_id: current_user.id, post_id: params[:post_id])
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new(comment_params)
+    @comment.post = @post
+    @comment.author = current_user
 
     if @comment.save
-      redirect_to "/users/#{current_user.id}/posts/#{params[:post_id]}"
+      redirect_to "/users/#{current_user.id}/comments", notice: 'Comment was successfully created.'
     else
       render 'posts/show'
     end
