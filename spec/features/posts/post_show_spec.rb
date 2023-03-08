2b3afcce1516818edb790178ff6d5a96a2c87fe5
Linus Shaddai <54178270+Linushaddai99@users.noreply.github.com>
2023-03-08 15:48:41 +0100

    Fix Linter errors

--- spec/features/posts/post_show_spec.rb
+++ spec/features/posts/post_show_spec.rb
@@ -3,43 +3,43 @@ require 'rails_helper'
 RSpec.describe 'posts#show', type: :feature do
   before :each do
     @user1 = User.create(name: 'Simon', photo: 'https://randomuser.me/api/portraits/men/1.jpg',
-                        bio: 'Teacher in South Africa', posts_counter: 23)
+                         bio: 'Teacher in South Africa', posts_counter: 23)
 
     @user2 = User.create(name: 'Peter', photo: 'https://randomuser.me/api/portraits/women/1.jpg',
-    bio: 'Teacher in Australia', posts_counter: 5)
+                         bio: 'Teacher in Australia', posts_counter: 5)
 
     @post1 = Post.create(author: @user1, title: 'My first blog',
-                        text: 'This is the blog 1 description that I wrote', comments_counter: 10, likes_counter: 82)
+                         text: 'This is the blog 1 description that I wrote', comments_counter: 10, likes_counter: 82)
 
     @comment1 = Comment.create(text: 'My first comment', author: @user2, post: @post1)
     visit user_post_path(@user1.id, @post1.id)
   end
 
-    it 'can see a posts title' do
-        expect(page).to have_content('My first blog')
-    end
+  it 'can see a posts title' do
+    expect(page).to have_content('My first blog')
+  end
 
-    it 'can see who wrote the post' do
-        expect(page).to have_content(@post1.author.name)
-    end
-    
-    it 'can see how many comments a post has' do
-        expect(page).to have_content(@post1.comments.count)
-    end
+  it 'can see who wrote the post' do
+    expect(page).to have_content(@post1.author.name)
+  end
 
-    it 'can see how many likes a post has' do
-        expect(page).to have_content(@post1.likes.count)
-    end
+  it 'can see how many comments a post has' do
+    expect(page).to have_content(@post1.comments.count)
+  end
 
-    it 'can see a posts body' do
+  it 'can see how many likes a post has' do
+    expect(page).to have_content(@post1.likes.count)
+  end
+
+  it 'can see a posts body' do
     expect(page).to have_content(@post1.text)
-    end
+  end
 
-    it 'can username of each commentor' do
+  it 'can username of each commentor' do
     expect(page).to have_content(@comment1.author.name)
-    end
+  end
 
-    it 'can see the comment text' do
+  it 'can see the comment text' do
     expect(page).to have_content(@comment1.text)
-    end
-end 
+  end
+end
